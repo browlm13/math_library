4dfe55376c17fbaa10b57d1ba1cac703bb9f0836
LJ Brown <browlm13@gmail.com>
2018-11-24 00:04:44 -0600

    Update eurlers_number.py

--- eurlers_number.py
+++ eurlers_number.py
@@ -14,8 +14,10 @@
 
 
 import math
+import numpy as np
 
-def p(n):
+
+def p1(n):
 
 	terms = []
 
@@ -26,7 +28,38 @@ def p(n):
 
 	return sum(terms)
 
+def p2(n):
+
+	d1 = [1/n**i for i in range(n+1)]
+	d2 = [1 / math.factorial(n-i) / math.factorial(i) for i in range(n+1)]
+
+	d = np.dot(d1, d2)
+	return math.factorial(n) * d
+
+
+def p3(n):
+
+	d0 = 1/np.array([n**i for i in range(n+1)])
+
+	d1a = 1/np.array([math.factorial(i) for i in range(n+1)])
+	d1b = np.flip(d1a)
+	d1 = np.multiply(d1a, d1b)
+
+	d = np.dot(d0, d1)
+
+	return math.factorial(n) * d
+
+from scipy.special import comb
+def p4(n):
+
+	t1 = 1/np.array([n**i for i in range(n+1)])
+	t2 = np.array([comb(n,i) for i in range(n+1)])
+	t = np.dot(t1,t2)
+
+	return t
+
 
 # check
 for i in range(171):
-	print("P(%s) = %s" % (i, p(i)))
+	print("P(%s) = %s" % (i, p4(i)))
+
